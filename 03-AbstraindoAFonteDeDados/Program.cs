fbbf922faea0ae19f4ab4e131f7c007d415fc2a8
Gustavo Pigaiani <gupigaiani@gmail.com>
2026-02-12 17:04:33 -0300

    Criação de outro csproj + lendo arquivo csv

---
+++ 03-AbstraindoAFonteDeDados/Program.cs
@@ -0,0 +1,41 @@
+﻿using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
+using var stream = new StreamReader(arquivo);
+
+var musicas = ObterMusicas(stream);
+ExibirMusicas(musicas);
+
+void ExibirMusicas(IEnumerable<Musica> musicas)
+{
+    var contador = 1;
+    foreach (var musica in musicas)
+    {
+        Console.WriteLine($"Título: {musica.Titulo} - Artista: {musica.Artista} - Duração: {musica.Duracao}");
+        contador++;
+        if (contador > 10) break;
+    }
+}
+
+IEnumerable<Musica> ObterMusicas(StreamReader stream)
+{
+    var linha = stream.ReadLine();
+    while (linha is not null)
+    {
+        var partes = linha.Split(';');
+        var musica = new Musica
+        {
+            Titulo = partes[0],
+            Artista = partes[1],
+            Duracao = Convert.ToInt32(partes[2])
+        };
+        yield return musica;
+        linha = stream.ReadLine();
+    }
+}
+
+class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+}
+
