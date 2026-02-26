2703f67cb5ff6bff6f4caa42c0030f585e886eb8
Gustavo Pigaiani <gupigaiani@gmail.com>
2026-02-26 17:10:31 -0300

    Trabalhando com LINQ - filtragem e ordenação

---
+++ 04-LINQ/Program.cs
@@ -0,0 +1,46 @@
+﻿using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
+using var stream = new StreamReader(arquivo);
+
+var musicasDoColdplay = 
+    ObterMusicas(stream)  
+    .Where(musica => musica.Artista == "Coldplay")
+    .OrderBy(musica => musica.Titulo)
+    .Skip(5 * 2)
+    .Take(5);
+
+ExibirMusicas(musicasDoColdplay);
+
+void ExibirMusicas(IEnumerable<Musica> musicas)
+{
+    var contador = 1;
+    foreach (var musica in musicas)
+    {
+        Console.WriteLine($"Título: {musica.Titulo} - Artista: {musica.Artista} - Duração: {musica.Duracao}");
+        contador++;
+        if (contador > 10) break;
+    }
+}
+
+IEnumerable<Musica> ObterMusicas(StreamReader stream)
+{
+    var linha = stream.ReadLine();
+    while (linha is not null)
+    {
+        var partes = linha.Split(';');
+        var musica = new Musica
+        {
+            Titulo = partes[0],
+            Artista = partes[1],
+            Duracao = Convert.ToInt32(partes[2])
+        };
+        yield return musica;
+        linha = stream.ReadLine();
+    }
+}
+
+class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+}
