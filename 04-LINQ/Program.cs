b682bfbbc541a91e9f99eb9cf12d012d7ef49c80
Gustavo Pigaiani <gupigaiani@gmail.com>
2026-02-26 17:49:56 -0300

    Adicionando metodo de estatisticas usando LINQ

--- 04-LINQ/Program.cs
+++ 04-LINQ/Program.cs
@@ -1,14 +1,31 @@
 ﻿using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
 using var stream = new StreamReader(arquivo);
 
-var generos = ObterMusicas(stream)
-    .SelectMany(m => m.Generos)
-    .Distinct()
-    .OrderBy(g => g);
+EstatisticasDeMusicas(stream);
 
-foreach (var genero in generos)
+void EstatisticasDeMusicas(StreamReader stream)
 {
-    Console.WriteLine(genero);
+    var musicas = ObterMusicas(stream).ToList();
+
+    Console.WriteLine($"\nExistem {musicas.Count()} músicas na coleção.");
+    Console.WriteLine($"\nExistem {musicas.Count(m => m.Duracao >= 600)} músicas com mais do que 10 minutos na coleção.");
+    Console.WriteLine($"\nA música com menor duração da coleção leva {musicas.Min(m => m.Duracao)} segundos");
+    Console.WriteLine($"\nA música com maior duração da coleção leva {musicas.Max(m => m.Duracao)} segundos");
+    Console.WriteLine($"\nA duração média das músicas da coleção é {musicas.Average(m => m.Duracao)} segundos");
+    Console.WriteLine($"\nVocê vai levar {musicas.Sum(m => m.Duracao)/(3600*24)} dias para ouvir toda a coleção!");
+}
+
+void OperacoesDeProjecao2(StreamReader stream)
+{
+    var generos = ObterMusicas(stream)
+        .SelectMany(m => m.Generos)
+        .Distinct()
+        .OrderBy(g => g);
+
+    foreach (var genero in generos)
+    {
+        Console.WriteLine(genero);
+    }
 }
 
 void OperacoesDeProjecao(StreamReader stream)
