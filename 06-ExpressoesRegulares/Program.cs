311a75f62c9ff94a5b9346191a2e148007c59601
Gustavo Pigaiani <gupigaiani@gmail.com>
2026-03-03 17:47:31 -0300

    Trabalhando com regex

---
+++ 06-ExpressoesRegulares/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Text.RegularExpressions;
+
+using var arquivo = new FileStream("musicas.csv", FileMode.Open, FileAccess.Read);
+using var stream = new StreamReader(arquivo);
+
+var musicas = ObterMusicas(stream)
+    .Take(20);
+
+ExibirMusicasEmTabela(musicas);
+
+void ExibirMusicas(IEnumerable<Musica> musicas)
+{
+    var titulo = "\nMúsicas do arquivo:"; // string literal
+    Console.WriteLine(titulo);
+    foreach (var musica in musicas)
+    {
+        var linha = $"\t- {musica.Titulo} ({musica.Artista}) - {musica.Duracao}s [{musica.Lancamento}]";
+        Console.WriteLine(linha);
+    }
+}
+
+void ExibirMusicasEmTabela(IEnumerable<Musica> musicas)
+{
+    var titulo = "\nMúsicas do arquivo:";
+    Console.WriteLine(titulo);
+
+    var colunaTitulo = "Titulo".PadRight(40);
+    var colunaArtista = "Artista".PadRight(30);
+    var colunaDuracao = "Duração".PadRight(10);
+    var colunaLancamento = "Lançada Em".PadRight(15);
+    Console.WriteLine($"{colunaTitulo}{colunaArtista}{colunaDuracao}{colunaLancamento}");
+    var borda = "".PadRight(100, '=');
+    Console.WriteLine(borda);
+
+    foreach (var musica in musicas)
+    {
+        var duracao = string.Format("{0,-10:F3}", musica.Duracao / 60.0);
+        var linha = $"{musica.Titulo,-40}{musica.Artista,-30}{duracao}{musica.Lancamento,-15:dd/MM/yyyy}";
+        Console.WriteLine(linha);
+    }
+}
+
+IEnumerable<Musica> ObterMusicas(StreamReader stream)
+{
+    var linha = stream.ReadLine();
+    while (linha is not null)
+    {
+        var partes = linha.Split(';');
+
+        int duracao = 350;
+        var match = Regex.Match(linha, @"(\d?\d):(\d\d)");
+        if (match.Success)
+        {
+            var minutos = int.Parse(match.Groups[1].Value);
+            var segundos = int.Parse(match.Groups[2].Value);
+            duracao = (minutos*60)+segundos;
+        }
+        if (partes.Length == 5)
+        {
+            var musica = new Musica
+            {
+                Titulo = string.IsNullOrWhiteSpace(partes[0]) ? "Título não encontrado" : partes[0],
+                Artista = string.IsNullOrWhiteSpace(partes[1]) ? "Artista não encontrado" : partes[1],
+                Duracao = duracao,
+                Generos = partes[3].Split(',', StringSplitOptions.TrimEntries),
+                Lancamento = DateTime.TryParse(partes[4], out var data) ? data : DateTime.Today
+            };
+            yield return musica;
+        }
+        linha = stream.ReadLine();
+    }
+    Console.WriteLine("Chegou ao fim do processamento!");
+}
+
+class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public IEnumerable<string> Generos { get; set; }
+    public DateTime Lancamento { get; set; }
+    public override string ToString()
+    {
+        return $"{Titulo} ({Artista}) - {Duracao}s [{Lancamento}]";
+    }
+}
